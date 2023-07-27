668ffa258ba0380728584e9557ae055f4eceb5be
Ifnlost1 <140715004+Ifnlost1@users.noreply.github.com>
2023-07-27 14:40:20 +0000

    correção

--- poo/teste.ts
+++ poo/teste.ts
@@ -6,10 +6,24 @@ const jose: User = {
     isdead: false
 }
 
-let joseconta = new BankAccount(jose, 1350.40);
+const agiota: User = {
+    name: "Carlos Alberto",
+    age: 48,
+    isdead: false
+}
+
+let joseconta: BankAccount = new BankAccount(jose, 1350.40);
 
 console.log(joseconta.ssaldo);
 joseconta.saldo = 36;
 console.log(joseconta.ssaldo)
 
+console.log(`Infelizmente, José é um homem bebâdo, e acabou fazendo um acordo com um agiota sem perceber :(
+    
+    O agiota, enfurecido, acabou matando o José, e roubou sua conta :(`)
 
+
+jose.isdead = true;
+joseconta.titular = agiota
+
+console.log(joseconta.titular)
